501765a966092c857097dcb1115f475b99f3b417
DG <dgasiorek14@gmail.com>
2024-07-24 17:33:58 +0200

    24.07/17:30

--- project/utils/views.py
+++ project/utils/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect
+
 import inspect, logging
 
 
@@ -16,8 +19,42 @@ def get_template(template_name: str = '', *, path: str = '', app: str) -> str:
 
 
 def get_func_logger() -> logging.Logger:
+    """Setups logger with the same name as the view name"""
+
     func_name = inspect.stack()[1][3]
     logger_name = f'logger_{func_name}'
     logger = logging.getLogger(logger_name)
 
     return logger
+
+
+def unauthenticated_user(redirect_to: str = ''):
+    """Restricts view for unauthenticated users only"""
+
+    def decorator(view_func):
+        def wrapper(request: HttpRequest, *args, **kwargs):
+            if request.user.is_authenticated:
+                return redirect(redirect_to)
+            else:
+                return view_func(request, *args, **kwargs)
+            
+        return wrapper
+    return decorator
+
+
+def allowed_users(allowed_rolse: list, redirect_to: str = ''):
+    """Restricts view for user with certain roles only"""
+
+    def decorator(view_func):
+        def wrapper(request: HttpRequest, *args, **kwargs):
+            group = None
+            if request.user.groups.exists():
+                group = request.user.groups.all()[0].name
+
+            if group in allowed_rolse:
+                return view_func(request, *args, **kwargs)
+            else:
+                return redirect(redirect_to)
+            
+        return wrapper
+    return decorator
